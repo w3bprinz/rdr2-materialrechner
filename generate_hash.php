e4fa3720b88f143e53ce1515d18bb8b517b58f44
Patrick <34249794+w3bprinz@users.noreply.github.com>
2024-11-09 20:12:18 +0100

    Anwendung der .gitignore

--- generate_hash.php
+++
@@ -1,5 +0,0 @@
-<?php
-$password = "IhrGewünschtesPasswort"; // Ändern Sie dies in Ihr gewünschtes Passwort
-$hash = password_hash($password, PASSWORD_DEFAULT);
-echo "Ihr Passwort-Hash: " . $hash;
-?> 
